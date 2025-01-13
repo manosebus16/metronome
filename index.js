1ddb1fe17357fa9a420195e4fee2223cd9fa40d9
Pranzal360 <pranzalkhatiwada03@gmail.com>
2025-01-13 23:11:08 +0545

    added meta images, fixed all the bugs regarding playing error and sound mismatch with beat

--- index.js
+++ index.js
@@ -1,69 +1,61 @@
-var delay = 1000;
-
-let interval
-
+let delay = 1000;
+let interval;
 let checkTick;
+
+let tom_1 = new Audio('sounds/tom-1.mp3');
+let crash = new Audio('sounds/crash.mp3');
+
 
 function beatValidation() {
 
-  let beatval = document.getElementById('beats').value
+    let beatval = document.getElementById('beats').value;
 
-  delay = 60000 / beatval
-
-  checkTick = document.getElementById('check').checked
-
-
-} 
-
+    delay = 60000 / beatval;
+}
 
 function startm() {
 
-  var tom_1 = new Audio("sounds/tom-1.mp3");
+    clonedTom = tom_1.cloneNode()
+    clonedCrash = crash.cloneNode()
 
-  var crash = new Audio("sounds/crash.mp3");
+    checkTick = document.getElementById('check').checked;
 
-  beatValidation()
+    let val = document.getElementById('count').innerText;
 
-  let val = document.getElementById('count').innerText
-  console.log(val);
+    val++;
 
-  val ++;
-  
-  if (val > 4) {
-    val = 1;
-  }
-  
-  if (val ==1 && checkTick == true){
-    crash.play()
-    console.info('1 is played')
-  }
+    // resetting the value of count to 1
+    if (val > 4) {
+        val = 1;
+    }
+    // if its first beat, play crash
+    if (val == 1 && checkTick == true) {
+        clonedCrash.play();
+        console.info('1 is played');
+   }
+    else {
+        clonedTom.play();
+    }
 
-  tom_1.play();
-
-  document.getElementById('count').innerText = val;
-
-  // btn restart
-
-  document.getElementById('btn').innerText = 'Restart'
-
-  document.getElementById('stop').hidden = false
-
+    // appending the value of count to the value of Val
+    document.getElementById('count').innerText = val;
+    document.getElementById('btn').hidden = true;
+    document.getElementById('stop').hidden = false;
 }
 
+function startfn() {
+    if (interval) {
+        clearInterval(interval);
+    }
 
-
-function startfn(){
-  interval = setInterval(startm,delay)
+    beatValidation();
+    interval = setInterval(startm, delay);
 }
-
 
 function stop() {
-
-  clearInterval(interval)
-
-  document.getElementById('btn').innerText = 'Click here to start metronome'
-  document.getElementById('count').innerText = 0;
-  document.getElementById('stop').hidden = true
-  
-
+    clearInterval(interval);
+    document.getElementById('btn').innerText = 'Start metronome';
+    document.getElementById('btn').hidden = false;
+    document.getElementById('count').innerText = ""
+    document.getElementById('stop').hidden = true;
 }
